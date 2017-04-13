657a5c5458a5cd5f8a8f641f1bf5715c871c23ef
Jordi Franco <jordi.franco@scmspain.com>
2017-04-13 23:42:28 +0200

    command line tool to retrieve windows password

--- connect.py
+++ connect.py
@@ -1,15 +1,20 @@
 from libs.ec2_adapter import Ec2Adapter
 from libs.rsa_decrypt import decryptPasswordWindows
+import argparse
 
-profile = 'your-profile'
-server_name = 'your-server-name'
 
-ec2client = Ec2Adapter(profile)
-instance_id = ec2client.get_ec2_instance_data(server_name)[0]['InstanceId']
-instance_dns = ec2client.get_ec2_instance_data(server_name)[0]['public_dns']
+parser = argparse.ArgumentParser(description='Creates a cloudfront distribution and related route53 needed')
+parser.add_argument('-p', '--profile', required=True, help='profile of the AWS account')
+args = parser.parse_args()
+__profile = args.profile
+__server_name = __profile
+
+ec2client = Ec2Adapter(__profile)
+instance_id = ec2client.get_ec2_instance_data(__server_name)[0]['InstanceId']
+instance_dns = ec2client.get_ec2_instance_data(__server_name)[0]['public_dns']
 
 password_data = ec2client.get_password_data(instance_id)
-decrypt = decryptPasswordWindows(profile=profile, password=password_data)
+decrypt = decryptPasswordWindows(profile=__profile, password=password_data)
 password = decrypt.decrypt_password()
 print(password)
 print(instance_id)
