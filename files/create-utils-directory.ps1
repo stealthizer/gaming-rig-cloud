2ce99a1dfe227461521b962a5b09249f81124852
Jordi Franco <jordi.franco@scmspain.com>
2017-04-16 22:16:13 +0200

    download the nvidia driver and put it on the utils directory

---
+++ files/create-utils-directory.ps1
@@ -0,0 +1,1 @@
+New-Item -ItemType Directory -Force -Path C:\utils
--- files/install-nvidia.ps1
+++ files/install-nvidia.ps1
@@ -1,8 +1,14 @@
-Write-Output "Downloading nvidia drivers"
-$nvidia_version = "372.90"
-$url = "http://es.download.nvidia.com/Windows/$($nvidia_version)/$($nvidia_version)-desktop-win10-32bit-international-whql.exe"
-$output = "$PSScriptRoot\nvidia_driver-$($nvidia_version).exe"
+$url = "http://us.download.nvidia.com/Windows/372.90/372.90-desktop-win8-win7-64bit-international-whql.exe"
+$output = "$PSScriptRoot\372.90-desktop-win8-win7-64bit-international-whql.exe"
+Write-Output "$PSScriptRoot"
 $start_time = Get-Date
 
-(New-Object System.Net.WebClient).DownloadFile($url, $output)
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Move-Item  "$PSScriptRoot\372.90-desktop-win8-win7-64bit-international-whql.exe" C:\utils\
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+Write-Output "unpacking nvidia driver"
+#check beyond this point
+$command = "C:\\utils\\372.90-desktop-win8-win7-64bit-international-whql.exe -s"
+iex $command
+dir C:\
